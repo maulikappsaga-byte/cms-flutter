f4d7f30656ddc9d2ea72f4ecc052044ff0e3d89e
Himanshu <himanshu.appsaga.io@gmail.com>
2026-06-11 15:33:31 +0530

    feat: add ApiConstants class to manage base URLs, API keys, and image resolution logic

--- cms_mobile/lib/constants/api_constants.dart
+++ cms_mobile/lib/constants/api_constants.dart
@@ -6,9 +6,9 @@ class ApiConstants {
   //     return 'http://10.0.2.2:8001/api';
   //   }
   //   return 'http://localhost:8001/api';
+  // }
 
-    static String get baseUrl => 'https://healthcare.appsaga.io/api';
-  }
+  static String get baseUrl => 'https://healthcare.appsaga.io/api';
 
   // static String apiKey = 'c7a6ba883d24c2f63e257245e36fe4bcbfebfe33ab8ca578f9ea8e8361056fee';
   static String apiKey = 'a467c9ae749554658c974ac9bdcdef787b9cc9ece425d33e2784e36c1aa37fc1';
